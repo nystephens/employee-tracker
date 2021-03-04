cf2f4de60169813721a7605c7b39b652b82e929f
Dead Nate <nathan.yukio.stephens@gmail.com>
2021-03-04 14:46:59 -0800

    seeds work for deparments table.  mock seed values for other tables in seeds.sql.  replace with real values and run

--- db/schema.sql
+++ db/schema.sql
@@ -6,29 +6,32 @@ DROP TABLE IF EXISTS departments;
 
 -- Departments
 CREATE TABLE departments (
-    id INT PRIMARY KEY,
-    department_name VARCHAR(30) NOT NULL
+    id INT NOT NULL AUTO_INCREMENT,
+    department_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 
 -- Roles
 CREATE TABLE roles (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(7,2),
     department_id INT,
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL,
+    PRIMARY KEY (id)
 );
 
 -- Employee
 CREATE TABLE employees (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
     CONSTRAINT fk_roles FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
-    CONSTRAINT fk_employees FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
+    CONSTRAINT fk_employees FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL,
+    PRIMARY KEY (id)
 );
 -- check if this logic works V
 -- manager_id INT to hold reference to another employee that is manager of the current employee. This field may be null if the employee has no manager.
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,27 @@
--- enter employee, role, and department information to populate tables
+INSERT INTO departments (department_name)
+VALUES
+("Bakery"),
+("Deli"),
+("Grocery"),
+("Frozen"),
+("Produce"),
+("Checkout");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+("title", "salary", "department_id"),
+("title", "salary", "department_id"),
+("title", "salary", "department_id"),
+("title", "salary", "department_id"),
+("title", "salary", "department_id"),
+("title", "salary", "department_id");
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+("first_name", "last_name", "role_id, manager_id"),
+("first_name", "last_name", "role_id, manager_id"),
+("first_name", "last_name", "role_id, manager_id"),
+("first_name", "last_name", "role_id, manager_id"),
+("first_name", "last_name", "role_id, manager_id"),
+("first_name", "last_name", "role_id, manager_id");
+
