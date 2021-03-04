07029dfc66426187b501d9706bb0a424864120cd
Dead Nate <nathan.yukio.stephens@gmail.com>
2021-03-04 11:16:54 -0800

    schema updated with all tables.  migrate script not working

--- db/schema.sql
+++ db/schema.sql
@@ -15,17 +15,18 @@ CREATE TABLE roles(
     title: VARCHAR(30) NOT NULL,
     salary: DECIMAL(p,s),
     department_id: INTEGER UNSIGNED,
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
+    CONSTRAINT fk_departments FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
 );
 
 -- Employee
-
--- id: INT PRIMARY KEY
-
--- first_name: VARCHAR(30) to hold employee first name
-
--- last_name: VARCHAR(30) to hold employee last name
-
--- role_id: INT to hold reference to employee role
-
+CREATE TABLE employees (
+    id: INTEGER PRIMARY KEY,
+    first_name: VARCHAR(30),
+    last_name: VARCHAR(30),
+    role_id: INTEGER UNSIGNED,
+    manager_id: INTEGER UNSIGNED,
+    CONSTRAINT fk_roles FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+    CONSTRAINT fk_employees FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
+);
+-- check if this logic works V
 -- manager_id: INT to hold reference to another employee that is manager of the current employee. This field may be null if the employee has no manager.
