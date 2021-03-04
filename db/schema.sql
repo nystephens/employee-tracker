29f84612729865f6c19d5313cad4ef22fd46ad44
Dead Nate <nathan.yukio.stephens@gmail.com>
2021-03-04 14:28:18 -0800

    schema runs smoothly

--- db/schema.sql
+++ db/schema.sql
@@ -1,32 +1,34 @@
-DROP TABLE IF EXISTS department;
-DROP TABLE IF EXISTS roles;
+USE employee_tracker;
+
 DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS departments;
 
 -- Departments
 CREATE TABLE departments (
-    id: INT PRIMARY KEY,
-    name: VARCHAR(30) NOT NULL
+    id INT PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL
 );
 
 
 -- Roles
-CREATE TABLE roles(
-    id: INT PRIMARY KEY,
-    title: VARCHAR(30) NOT NULL,
-    salary: DECIMAL(p,s),
-    department_id: INTEGER UNSIGNED,
-    CONSTRAINT fk_departments FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
+CREATE TABLE roles (
+    id INT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(7,2),
+    department_id INT,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
 );
 
 -- Employee
 CREATE TABLE employees (
-    id: INTEGER PRIMARY KEY,
-    first_name: VARCHAR(30),
-    last_name: VARCHAR(30),
-    role_id: INTEGER UNSIGNED,
-    manager_id: INTEGER UNSIGNED,
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
     CONSTRAINT fk_roles FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
     CONSTRAINT fk_employees FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
 );
 -- check if this logic works V
--- manager_id: INT to hold reference to another employee that is manager of the current employee. This field may be null if the employee has no manager.
+-- manager_id INT to hold reference to another employee that is manager of the current employee. This field may be null if the employee has no manager.
