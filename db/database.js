7494bf54afba86e2572b64d732778228a5220784
Dead Nate <nathan.yukio.stephens@gmail.com>
2021-03-05 10:52:03 -0800

    question arrays created.  master switch created

--- db/database.js
+++ db/database.js
@@ -1,2 +1,75 @@
-// this js file will house the connection.query functions for the app
-// use console.table to format results
+const inquirer = require('inquirer');
+const msyql = require('mysql2');
+const format = require('console.table');
+// this file will contain the database queries
+
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'employee_tracker'
+});
+
+
+
+// // this js file will house the connection.query functions for the app
+// // use console.table to format results
+
+function getEmployeeByRoles() {
+    connection.query(
+        'SELECT * FROM `employee` WHERE `role_id` = 6',
+        function (err, results,) {
+            console.log(results); // results contains rows returned by server
+        }
+    );
+};
+
+getEmployeeByRoles();
+
+// switch statment that has initial options as cases.  each case will contain the appropriate query for viewing table results.  if edit table options selected, then additional questions will fire.
+
+function masterswitch() {
+    switch (answers) {
+        case "View All Departments":
+            // query to view departments
+            break;
+
+        case "View All Roles":
+            // query to view roles
+            break;
+
+        case "View All Employees":
+            // query to view employees
+            break;
+
+        case "Add a Department":
+            // call addDepartment();
+            break;
+
+        case "Add a Role":
+            // call addRole();
+            break;
+
+        case "Add an Employee":
+            // call addEmployee();
+            break;
+
+        case "Update Employee Role":
+            // call updateEmpRole();
+            break;
+
+        case "Exit Program":
+            // exit program
+            console.log("Goodbye!");
+            break;
+
+        default:
+            console.log("No option selected.");
+            break;
+    }
+};
+
+// module.exports = 
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,8 @@
+USE employee_tracker;
+
 INSERT INTO departments (department_name)
 VALUES
+("Management"),
 ("Bakery"),
 ("Deli"),
 ("Grocery"),
@@ -9,19 +12,34 @@ VALUES
 
 INSERT INTO roles (title, salary, department_id)
 VALUES
-("title", "salary", "department_id"),
-("title", "salary", "department_id"),
-("title", "salary", "department_id"),
-("title", "salary", "department_id"),
-("title", "salary", "department_id"),
-("title", "salary", "department_id");
+("General Manager", 80000.00, 1),
+("Assistant Store Manager", 60000.00, 1),
+("Produce Manager", 50000.00, 6),
+("Grocery Manager", 50000.00, 4),
+("Frozen Goods Manager", 50000.00, 5),
+("Checkout Clerk", 20000.00, 7),
+("Grocery Crew", 20000.00, NULL),
+("Baker", 35000.00, 2),
+("Butcher", 40000.00, 3);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
-("first_name", "last_name", "role_id, manager_id"),
-("first_name", "last_name", "role_id, manager_id"),
-("first_name", "last_name", "role_id, manager_id"),
-("first_name", "last_name", "role_id, manager_id"),
-("first_name", "last_name", "role_id, manager_id"),
-("first_name", "last_name", "role_id, manager_id");
+("Aprul", "Diaz", 1, NULL),
+("Gill", "Frankel", 2, 1),
+("Sherry", "McDonald", 3, 2),
+("Katie", "Cormack", 4, 2),
+("Kendall", "Mancino", 5, 2),
+("Mark", "Rowley", 7, 4),
+("Aisha", "Motumbo", 7, 4),
+("Amy", "Grant", 7, 3),
+("Hideki", "Matsui", 6, 4),
+("Peirs", "Fern", 6, 4),
+("Hugh", "Jass", 7, 5),
+("Michelle", "Myers", 7, 5),
+("Carolyn", "Ferarro", 8, 2),
+("James", "Shanty", 9, 2),
+("Mike", "Hunt", 7, 13),
+("Lindsey", "Tauiliili", 6, 4),
+("Vance", "Gerardo", 7, 13),
+("Onuabe", "Collins", 6, 14);
 
